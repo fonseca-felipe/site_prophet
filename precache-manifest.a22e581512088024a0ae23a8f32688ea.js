5b8ae4cbcfff3e250144c3bfe374bd3a5ecc5427
fonseca-felipe <felipe-fmc@hotmail.com>
2021-05-21 15:17:27 -0300

    Updates

---
+++ precache-manifest.a22e581512088024a0ae23a8f32688ea.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3727d34952b2f47d3a948e328188370",
+    "url": "/site_prophet/index.html"
+  },
+  {
+    "revision": "243600bee38fca6cede7",
+    "url": "/site_prophet/static/css/2.a5f06b7f.chunk.css"
+  },
+  {
+    "revision": "d0055b1298aa6cd0adb0",
+    "url": "/site_prophet/static/css/main.e74965db.chunk.css"
+  },
+  {
+    "revision": "243600bee38fca6cede7",
+    "url": "/site_prophet/static/js/2.796c5a2c.chunk.js"
+  },
+  {
+    "revision": "d0055b1298aa6cd0adb0",
+    "url": "/site_prophet/static/js/main.43153a08.chunk.js"
+  },
+  {
+    "revision": "5eb9920ece6703cd58f4",
+    "url": "/site_prophet/static/js/runtime~main.08d57431.js"
+  },
+  {
+    "revision": "df5067ed7d655b3a49a8d34493847c19",
+    "url": "/site_prophet/static/media/Cardio.df5067ed.png"
+  },
+  {
+    "revision": "abef84a190c1ca14e9dbff23004fefdf",
+    "url": "/site_prophet/static/media/English.abef84a1.png"
+  },
+  {
+    "revision": "6bf2ad43ba460515e7804f9de779006d",
+    "url": "/site_prophet/static/media/NIHSS.6bf2ad43.jpg"
+  },
+  {
+    "revision": "40d6d63741178c0e0bec5955073f5366",
+    "url": "/site_prophet/static/media/Portugues.40d6d637.png"
+  },
+  {
+    "revision": "c2080d87890be4809f1a9075eb0a3904",
+    "url": "/site_prophet/static/media/Spanish.c2080d87.png"
+  },
+  {
+    "revision": "e11bd6f9e496acfefab71c627015168b",
+    "url": "/site_prophet/static/media/capes.e11bd6f9.jpg"
+  },
+  {
+    "revision": "b3c708a5f41e6b58b14052cd99d15fb0",
+    "url": "/site_prophet/static/media/glycemia.b3c708a5.png"
+  },
+  {
+    "revision": "f82c9b551bf7233b2ce97d98239715eb",
+    "url": "/site_prophet/static/media/logo.f82c9b55.png"
+  },
+  {
+    "revision": "163fd6b6be34f406b30a1774aaa1d8dd",
+    "url": "/site_prophet/static/media/logo_prophet_pc.163fd6b6.png"
+  },
+  {
+    "revision": "2c5896c16cb635ee433aaec108efeb95",
+    "url": "/site_prophet/static/media/male.2c5896c1.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.62480aa4ef0f178ed751902cb79f862c.js"
+  "/site_prophet/precache-manifest.a22e581512088024a0ae23a8f32688ea.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/site_prophet/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
