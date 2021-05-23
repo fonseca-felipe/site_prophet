55b557b526619ca452e6bb489430c68f27caba16
fonseca-felipe <84293186+fonseca-felipe@users.noreply.github.com>
2021-05-23 19:33:51 -0300

    Update messages.js

--- src/messages.js
+++ src/messages.js
@@ -15,17 +15,17 @@ export default {
         'microangiopathy':'Leukoaraiosis', 
         'microangiopathy.description':'<a href="https://www.ncbi.nlm.nih.gov/pubmed/3496763">FAZEKAS grade (from 1 to 6) (Fazekas, 1987) </a> was considered as leukoaraiosis', 
 
-        'nihss':"Nihss",
-        'nihss.description':'A descrição do nihss',
+        'nihss':"NIHSS",
+        'nihss.description':'NIHSS',
 
-        'cardio': "Cárdio", 
+        'cardio': "TOAST Cardio-aortic", 
         'cardio.description': 'a descrição do cárdio',
 
         'lacunar_syndrome':'Lacunar syndrome', 
         'lacunar_syndrome.description':'Defined based on The Oxfordshire Community Stroke Project classification system <a href="https://www.ncbi.nlm.nih.gov/pubmed/1675378">(Bamford, 1991)</a>: pure motor OR pure sensory OR sensorimotor symptoms OR ataxic hemiparesis OR clumsy-hand dysarthria <br/> <b>Visit our computerized version of The Oxfordshire Community Stroke Project classification system at  <a href="http://www.compact-stroke.com">www.compact-stroke.com</a> </b> ',        
 
-        'aortic_insufficiency':'Cardio-aortic embolism',
-        'aortic_insufficiency.description':`We attested the stroke mechanism Cardio-aortic embolism from <a href="https://www.ncbi.nlm.nih.gov/pubmed/16240340">SSS-TOAST</a>, an evidence-based system. In our score, we considered all the levels of evidence described in the system. <br/><br/>
+        'cardio': "TOAST Cardio-aortic", 
+        'cardio.description':`We attested the stroke mechanism Cardio-aortic embolism from <a href="https://www.ncbi.nlm.nih.gov/pubmed/16240340">SSS-TOAST</a>, an evidence-based system. In our score, we considered all the levels of evidence described in the system. <br/><br/>
             <b>The presence of a high-risk cardiac source of cerebral embolism (Evident level of confidence):</b><br/><br/>
             Sources of embolism of thrombotic origin <br/>
             1. Left atrial thrombus <br/>
@@ -65,7 +65,7 @@ export default {
                                 Hosmer-Lemeshow 0.86 <br/>
                                 Sensibility: 78 (68-86)% <br/> 
                                 Specificity 75 (70-79)% <br/>
-                                Sensibility  (symptomatic cases of HT): 78 (62.2-89.4)% <br/>
+                                Sensibility  (symptomatic cases of HT): 78 (62.2-89.4)% <br/>
                                 Specificity (symptomatic cases of HT): 75 (70-80)% <br/>
                                 OR (General HT): 10.6 (95% CI 6-18.6, p &lt; 0.001) <br/>
                                 OR (Symptomatic HT): 10.7 (95% CI, 4.9-23.3, p &lt; 0.001) <br/>`,
@@ -94,14 +94,14 @@ export default {
         'lacunar_syndrome':'Síndrome Lacunar',
         'lacunar_syndrome.description':'Definida de acordo com o The Oxfordshire Community Stroke Project classification system <a href="https://www.ncbi.nlm.nih.gov/pubmed/1675378">(Bamford, 1991)</a>: sintoma motor puro OU sensitivo puro OU sensitivo-motor OU hemiparesia atáxica OU disartria clumsy-hand. <br/> <b>Visite nosso sistema automatizado de classificação do The Oxfordshire Community Stroke Project classification system em <a href="http://www.compact-stroke.com">www.compact-stroke.com</a> (disponivel apenas em Inglês) </b>',
         
-        'nihss':"Nihss",
-        'nihss.description':'A descrição do nihss',
+        'nihss':"NIHSS",
+        'nihss.description':'NIHSS',
 
         'cardio': "Cárdio", 
         'cardio.description': 'a descrição do cárdio',
 
-        'aortic_insufficiency':'Etiologia Cárdio-aórtica',
-        'aortic_insufficiency.description':`Foi classificado como mecanismo etiológico cardio-aórtico segundo os critérios propostos no sistema de classificação <a href="https://www.ncbi.nlm.nih.gov/pubmed/16240340">SSS-TOAST</a>. Todos os níveis de confiança foram considerados.</a> <br/><br/>
+        'cardio': "TOAST Cardio-aortic", 
+        'cardio.description':`Foi classificado como mecanismo etiológico cardio-aórtico segundo os critérios propostos no sistema de classificação <a href="https://www.ncbi.nlm.nih.gov/pubmed/16240340">SSS-TOAST</a>. Todos os níveis de confiança foram considerados.</a> <br/><br/>
             <b>Presença de uma fonte de embolia cerebral de alto risco (Nível Evidente) </b> <br/><br/>
             1. Trombo no átrio esquerdo <br/>
             2. Trombo no ventrículo esquerdo <br/>
@@ -167,46 +167,46 @@ export default {
         'microangiopathy.description':'<a href="https://www.ncbi.nlm.nih.gov/pubmed/3496763">Los grados 1 a 6 en la clasificación de Fazekas (Fazekas, 1987)</a> fueron considerados como presencia de microangiopatia',
 
         'lacunar_syndrome':'Síndrome lacunar',
-        'lacunar_syndrome.description':'Definido según el “The Oxfordshire Community Stroke Project classification system” <a href="https://www.ncbi.nlm.nih.gov/pubmed/1675378">(Bamford, 1991)</a>: síntomas puramente motores O sensoriales O sensoriomotores puros O hemiparesia atáxica O disartria “clumsy-hand”. <br/> <b>Visite nuestro sistema automatizado de clasificación del Proyecto Oxfordshire Community Stroke en <a href="http://www.compact-stroke.com">www.compact-stroke.com</a> </b>',
+        'lacunar_syndrome.description':'Definido según el “The Oxfordshire Community Stroke Project classification system” <a href="https://www.ncbi.nlm.nih.gov/pubmed/1675378">(Bamford, 1991)</a>: síntomas puramente motores O sensoriales O sensoriomotores puros O hemiparesia atáxica O disartria “clumsy-hand”. <br/> <b>Visite nuestro sistema automatizado de clasificación del Proyecto Oxfordshire Community Stroke en <a href="http://www.compact-stroke.com">www.compact-stroke.com</a> </b>',
 
-        'nihss':"Nihss",
-        'nihss.description':'A descrição do nihss',
+        'nihss':"NIHSS",
+        'nihss.description':'NIHSS',
 
         'cardio': "Cárdio", 
         'cardio.description': 'a descrição do cárdio',
 
-        'aortic_insufficiency':'Embolismo cardio-aórtico',
-        'aortic_insufficiency.description':`
-                Hemos evaluado el mecanismo cardioembólico de accidente cerebrovascular a partir de la clasificación basada en evidencia <a href="https://www.ncbi.nlm.nih.gov/pubmed/16240340">SSS-TOAST</a>. En nuestra puntuación, consideramos todos los niveles de evidencia descritos en la clasificación. <br/><br/>
+        'cardio': "TOAST Cardio-aortic", 
+        'cardio.description':`
+                Hemos evaluado el mecanismo cardioembólico de accidente cerebrovascular a partir de la clasificación basada en evidencia <a href="https://www.ncbi.nlm.nih.gov/pubmed/16240340">SSS-TOAST</a>. En nuestra puntuación, consideramos todos los niveles de evidencia descritos en la clasificación. <br/><br/>
                 <b>Presencia de embolismo cerebral de fuente cardíaca de alto riesgo (Nivel de confianza evidente):</b> <br/><br/>
-                <i>Fuentes de embolismo de origen trombótico </i> <br/><br/>
-                1.     Trombo de aurícula izquierda <br/>
-                2.     Trombo de ventrículo izquierdo <br/>
-                3.     Fibrilación auricular <br/>
-                4.     Fibrilación auricular paroxística <br/>
-                5.     Síndrome del seno enfermo <br/>
-                6.     Flutter auricular sostenido <br/>
-                7.     Infarto agudo de miocardio reciente (Dentro de un mes) <br/>
-                8.     Enfermedad valvular reumática mitral o aórtica <br/>
-                9.     Presencia de válvulas cardíacas mecánicas o bioprotésicas <br/>
-                10.  Infarto agudo de miocardio crónico asociado a fracción de eyección reducida menor del 28% <br/>
-                11.  Insuficiencia cardíaca congestiva sintomática con fracción de eyección menor del 30% <br/>
-                12.  Miocardiopatía dilatada <br/>
-                13.  Endocarditis trombótica no bacteriana  <br/><br/>
+                <i>Fuentes de embolismo de origen trombótico </i> <br/><br/>
+                1.     Trombo de aurícula izquierda <br/>
+                2.     Trombo de ventrículo izquierdo <br/>
+                3.     Fibrilación auricular <br/>
+                4.     Fibrilación auricular paroxística <br/>
+                5.     Síndrome del seno enfermo <br/>
+                6.     Flutter auricular sostenido <br/>
+                7.     Infarto agudo de miocardio reciente (Dentro de un mes) <br/>
+                8.     Enfermedad valvular reumática mitral o aórtica <br/>
+                9.     Presencia de válvulas cardíacas mecánicas o bioprotésicas <br/>
+                10.  Infarto agudo de miocardio crónico asociado a fracción de eyección reducida menor del 28% <br/>
+                11.  Insuficiencia cardíaca congestiva sintomática con fracción de eyección menor del 30% <br/>
+                12.  Miocardiopatía dilatada <br/>
+                13.  Endocarditis trombótica no bacteriana  <br/><br/>
                 <i>Fuentes de embolismo sin origen trombótico predominante</i> <br/> <br/>
-                14.  Endocarditis infecciosa <br/>
-                15.  Fibroelastoma papilar <br/>
-                16.  Mixoma de aurícula izquierda  <br/><br/>
-                <b>Evidencia de embolismo sistémico o presencia de múltiples infartos agudos que ocurrieron estrechamente relacionados en tiempo en territorios de circulación anterior bilateral o en circulación anterior y posterior simultáneamente; en ausencia de: oclusión o estenosis crítica de vasos relacionados topográficamente, otras enfermedades que pueden causar lesiones isquémicas multifocales como: vasculitis, vasculopatías o alteraciones hemostáticas o hemodinámicas. (Nivel de confianza probable) </b><br/><br/>
-                <b>La presencia de uma condición cardíaca con riesgo primario  bajo o incierto para embolismo cerebral (ver abajo) o evidencia de embolismo cardioaórtico evidente en ausencia de investigación etiológica completa para otros mecanismos. (Nivel de confianza posible) </b> <br/><br/>
-                <i>Fuentes de riesgo primario bajo o incierto para accidente cerebrovascular isquémico </i> <br/><br/>
-                17.  Calcificación anular mitral <br/>
-                18.  Foramen ovale permeable <br/>
-                19.  Aneurisma de septum auricular <br/>
-                20.   Aneurisma de septum auricular y foramen ovale permeable <br/>
-                21.  Aneruisma ventricular izquierdo sin trombo intracavitario <br/>
-                22.  Contraste ecocardiográfico espontáneo auricular izquierdo aislado (sin estenosis mitral ni fibrilación auricular) <br/>
-                23.  Ateroma complicado en la aorta ascendente o en el arco aórtico proximal <br/>
+                14.  Endocarditis infecciosa <br/>
+                15.  Fibroelastoma papilar <br/>
+                16.  Mixoma de aurícula izquierda  <br/><br/>
+                <b>Evidencia de embolismo sistémico o presencia de múltiples infartos agudos que ocurrieron estrechamente relacionados en tiempo en territorios de circulación anterior bilateral o en circulación anterior y posterior simultáneamente; en ausencia de: oclusión o estenosis crítica de vasos relacionados topográficamente, otras enfermedades que pueden causar lesiones isquémicas multifocales como: vasculitis, vasculopatías o alteraciones hemostáticas o hemodinámicas. (Nivel de confianza probable) </b><br/><br/>
+                <b>La presencia de uma condición cardíaca con riesgo primario  bajo o incierto para embolismo cerebral (ver abajo) o evidencia de embolismo cardioaórtico evidente en ausencia de investigación etiológica completa para otros mecanismos. (Nivel de confianza posible) </b> <br/><br/>
+                <i>Fuentes de riesgo primario bajo o incierto para accidente cerebrovascular isquémico </i> <br/><br/>
+                17.  Calcificación anular mitral <br/>
+                18.  Foramen ovale permeable <br/>
+                19.  Aneurisma de septum auricular <br/>
+                20.   Aneurisma de septum auricular y foramen ovale permeable <br/>
+                21.  Aneruisma ventricular izquierdo sin trombo intracavitario <br/>
+                22.  Contraste ecocardiográfico espontáneo auricular izquierdo aislado (sin estenosis mitral ni fibrilación auricular) <br/>
+                23.  Ateroma complicado en la aorta ascendente o en el arco aórtico proximal <br/>
         `,
         'quiz.click':'*Haga clic en los cuadros para más información',
 
