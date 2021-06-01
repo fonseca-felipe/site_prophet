2712826ca4edac594cb8ba3198be330e37ae8cb2
fonseca-felipe <felipe-fmc@hotmail.com>
2021-06-01 11:01:38 -0300

    Updates

---
+++ precache-manifest.7ea45bbab6a253c66525d3082ca2ab86.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8af4975db411f6e71dd72d2ce75b35e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27c3ff9e965910bec02c",
+    "url": "/static/css/2.a5f06b7f.chunk.css"
+  },
+  {
+    "revision": "7c26aaea1629da273426",
+    "url": "/static/css/main.e74965db.chunk.css"
+  },
+  {
+    "revision": "27c3ff9e965910bec02c",
+    "url": "/static/js/2.ab01ca9f.chunk.js"
+  },
+  {
+    "revision": "7c26aaea1629da273426",
+    "url": "/static/js/main.c6f4d2a7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "df5067ed7d655b3a49a8d34493847c19",
+    "url": "/static/media/Cardio.df5067ed.png"
+  },
+  {
+    "revision": "abef84a190c1ca14e9dbff23004fefdf",
+    "url": "/static/media/English.abef84a1.png"
+  },
+  {
+    "revision": "6bf2ad43ba460515e7804f9de779006d",
+    "url": "/static/media/NIHSS.6bf2ad43.jpg"
+  },
+  {
+    "revision": "40d6d63741178c0e0bec5955073f5366",
+    "url": "/static/media/Portugues.40d6d637.png"
+  },
+  {
+    "revision": "c2080d87890be4809f1a9075eb0a3904",
+    "url": "/static/media/Spanish.c2080d87.png"
+  },
+  {
+    "revision": "e11bd6f9e496acfefab71c627015168b",
+    "url": "/static/media/capes.e11bd6f9.jpg"
+  },
+  {
+    "revision": "b3c708a5f41e6b58b14052cd99d15fb0",
+    "url": "/static/media/glycemia.b3c708a5.png"
+  },
+  {
+    "revision": "f82c9b551bf7233b2ce97d98239715eb",
+    "url": "/static/media/logo.f82c9b55.png"
+  },
+  {
+    "revision": "163fd6b6be34f406b30a1774aaa1d8dd",
+    "url": "/static/media/logo_prophet_pc.163fd6b6.png"
+  },
+  {
+    "revision": "2c5896c16cb635ee433aaec108efeb95",
+    "url": "/static/media/male.2c5896c1.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/site_prophet/precache-manifest.42f5d152b1cf4870982f0b55324a0484.js"
+  "/precache-manifest.7ea45bbab6a253c66525d3082ca2ab86.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/site_prophet/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
