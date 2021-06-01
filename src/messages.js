a123c269dcc23dfab0c42f0d7a7f3915809722c4
fonseca-felipe <felipe-fmc@hotmail.com>
2021-06-01 11:12:24 -0300

    getting paths right

--- src/messages.js
+++ src/messages.js
@@ -15,12 +15,41 @@ export default {
         'microangiopathy':'Leukoaraiosis', 
         'microangiopathy.description':'<a href="https://www.ncbi.nlm.nih.gov/pubmed/3496763">FAZEKAS grade (from 1 to 6) (Fazekas, 1987) </a> was considered as leukoaraiosis', 
 
-        'nihss':"Nihss",
-        'nihss.description':'A descrição do nihss',
+        'nihss':"NIHSS",
+        'nihss.description':'NIHSS',
 
-        'cardio': "Cárdio", 
-        'cardio.description': 'a descrição do cárdio',
-
+        'cardio': "TOAST Cardio-aortic", 
+        'cardio.description': `We attested the stroke mechanism Cardio-aortic embolism from <a href="https://www.ncbi.nlm.nih.gov/pubmed/16240340">SSS-TOAST</a>, an evidence-based system. In our score, we considered all the levels of evidence described in the system. <br/><br/>
+        <b>The presence of a high-risk cardiac source of cerebral embolism (Evident level of confidence):</b><br/><br/>
+        Sources of embolism of thrombotic origin <br/>
+        1. Left atrial thrombus <br/>
+        2. Left ventricular thrombus <br/>
+        3. Atrial fibrillation <br/>
+        4. Paroxysmal atrial fibrillation <br/>
+        5. Sick sinus syndrome <br/>
+        6. Sustained atrial flutter <br/>
+        7. Recent myocardial infarction (within 1 month) <br/>
+        8. Rheumatoid mitral or aortic valve disease <br/>
+        9. Bioprosthetic and mechanical heart valves <br/>
+        10. Chronic myocardial infarction together with low ejection fraction less than 28% <br/>
+        11. Symptomatic congestive heart failure with ejection fraction less than 30% <br/>
+        12. Dilated cardiomyopathy <br/>
+        13. Nonbacterial thrombotic endocarditis <br/><br/>
+        Sources with embolism not predominantly of thrombotic origin <br/><br/>
+        14. Infective endocarditis <br/>
+        15. Papillary fibroelastoma <br/>
+        16. Left atrial myxoma <br/><br/>
+        <b>Evidence of systemic embolism or presence of multiple acute infarctions that have occurred closely related in time within both right and left anterior or both anterior and posterior circulations in the absence of occlusion or near-occlusive stenosis of all relevant vessels; other diseases that can cause multifocal ischemic brain injury such as vasculitis, vasculopathies, and hemostatic or hemodynamic disturbances must not be present (Probable level of confidence)</b> <br/><br/>
+        <b>The presence of a cardiac condition with low or uncertain primary risk of cerebral embolism (see below) or Evidence for evident cardioaortic embolism in the absence of complete diagnostic investigation for other mechanisms (Possible level of confidence)</b> <br/><br/>
+        <i> Sources with low or uncertain primary risk for ischemic stroke </i><br/><br/>
+        17. Mitral annular calcification <br/>
+        18. Patent foramen ovale <br/>
+        19. Atrial septal aneurysm <br/>
+        20. Atrial septal aneurysm and patent foramen ovale <br/>
+        21. Left ventricular aneurysm without thrombus <br/>
+        22. Isolated left atrial smoke (no mitral stenosis or atrial fibrillation) <br/>
+        23. Complex atheroma in the ascending aorta or proximal arch`,             
+    'quiz.click':'*Click on the boxes for more information',
         'lacunar_syndrome':'Lacunar syndrome', 
         'lacunar_syndrome.description':'Defined based on The Oxfordshire Community Stroke Project classification system <a href="https://www.ncbi.nlm.nih.gov/pubmed/1675378">(Bamford, 1991)</a>: pure motor OR pure sensory OR sensorimotor symptoms OR ataxic hemiparesis OR clumsy-hand dysarthria <br/> <b>Visit our computerized version of The Oxfordshire Community Stroke Project classification system at  <a href="http://www.compact-stroke.com">www.compact-stroke.com</a> </b> ',        
 
@@ -94,11 +123,40 @@ export default {
         'lacunar_syndrome':'Síndrome Lacunar',
         'lacunar_syndrome.description':'Definida de acordo com o The Oxfordshire Community Stroke Project classification system <a href="https://www.ncbi.nlm.nih.gov/pubmed/1675378">(Bamford, 1991)</a>: sintoma motor puro OU sensitivo puro OU sensitivo-motor OU hemiparesia atáxica OU disartria clumsy-hand. <br/> <b>Visite nosso sistema automatizado de classificação do The Oxfordshire Community Stroke Project classification system em <a href="http://www.compact-stroke.com">www.compact-stroke.com</a> (disponivel apenas em Inglês) </b>',
         
-        'nihss':"Nihss",
-        'nihss.description':'A descrição do nihss',
+        'nihss':"NIHSS",
+        'nihss.description':'NIHSS',
 
-        'cardio': "Cárdio", 
-        'cardio.description': 'a descrição do cárdio',
+        'cardio': "TOAST Cardio-aortic", 
+        'cardio.description': `Foi classificado como mecanismo etiológico cardio-aórtico segundo os critérios propostos no sistema de classificação <a href="https://www.ncbi.nlm.nih.gov/pubmed/16240340">SSS-TOAST</a>. Todos os níveis de confiança foram considerados.</a> <br/><br/>
+        <b>Presença de uma fonte de embolia cerebral de alto risco (Nível Evidente) </b> <br/><br/>
+        1. Trombo no átrio esquerdo <br/>
+        2. Trombo no ventrículo esquerdo <br/>
+        3. Fibrilação atrial <br/>
+        4. Fibrilação atrial paroxística <br/>
+        5. Síndrome do nó sinusal <br/>
+        6. Flutter atrial sustentado <br/>
+        7. Infarto do miocárdio recente (dentro de 1 mês) <br/>
+        8. Doença reumática nas valva mitral ou aórtica <br/>
+        9. Valva biológica ou mecânica <br/>
+        10. Infarto crônico do miocárdio com fração de ejeção inferior a 28% <br/>
+        11. Insuficiência cardíaca sintomática com fração de ejeção inferior a 30% <br/>
+        12. Cardiomiopatia dilatada <br/>
+        13. Endocardite trombótica não infecciosa <br/><br/>
+        <i>Fonte de origem não predominantemente trombótica </i> <br/>
+        14. Endocardite infeciosa <br/>
+        15. Fibroelastoma papilar <br/>
+        16. Mixoma atrial esquerdo <br/><br/>
+        <b>Evidência de embolia sistêmica ou presença de múltiplos infartos agudos que ocorreram quase ao mesmo tempo bilateralmente na circulação anterior ou posterior, na ausência de oclusão ou estenose crítica em todos os vasos relevantes; outras condições que causem isquemia multifocal como vasculite, vasculopatias e distúrbios de coagulação ou hemodinâmicos devem não estar presentes. (Nível Provável) </b> </br> </br>
+        <b>A presença de uma condição cardíaca com baixo ou incerto risco primário de embolia cerebral (veja abaixo) ou evidência de evidente embolia cardioaórtica na ausência de investigação diagnóstica completa para outros mecanismos. (Nível Possível)</b> </br></br>
+        <i>Fontes com baixo ou incerto risco primário para AVC isquêmico </i> </br></br>
+        17. Calcificação do anel mitral </br>
+        18. Forame oval patente </br>
+        19. Aneurisma de septo atrial </br>
+        20. Aneurisma de septo atrial com forame oval patente </br>
+        21. Aneurisma de ventrículo esquerdo sem trombo </br>
+        22. Sombra isolada no átrio esquerdo (sem estenose mitral ou fibrilação atrial) </br>
+        23. Ateroma complexo na aorta ascendente ou arco proximal `,
+    'quiz.click': '*Clique nas caixas para mais informações',
 
         'aortic_insufficiency':'Etiologia Cárdio-aórtica',
         'aortic_insufficiency.description':`Foi classificado como mecanismo etiológico cardio-aórtico segundo os critérios propostos no sistema de classificação <a href="https://www.ncbi.nlm.nih.gov/pubmed/16240340">SSS-TOAST</a>. Todos os níveis de confiança foram considerados.</a> <br/><br/>
@@ -169,10 +227,10 @@ export default {
         'lacunar_syndrome':'Síndrome lacunar',
         'lacunar_syndrome.description':'Definido según el “The Oxfordshire Community Stroke Project classification system” <a href="https://www.ncbi.nlm.nih.gov/pubmed/1675378">(Bamford, 1991)</a>: síntomas puramente motores O sensoriales O sensoriomotores puros O hemiparesia atáxica O disartria “clumsy-hand”. <br/> <b>Visite nuestro sistema automatizado de clasificación del Proyecto Oxfordshire Community Stroke en <a href="http://www.compact-stroke.com">www.compact-stroke.com</a> </b>',
 
-        'nihss':"Nihss",
-        'nihss.description':'A descrição do nihss',
+        'nihss':"NIHSS",
+        'nihss.description':'NIHSS',
 
-        'cardio': "Cárdio", 
+        'cardio': "TOAST Cardio-aortic", 
         'cardio.description': 'a descrição do cárdio',
 
         'aortic_insufficiency':'Embolismo cardio-aórtico',
